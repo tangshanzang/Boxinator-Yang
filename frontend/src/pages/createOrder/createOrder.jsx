e51ba6435492dd33098ce6f80882bf1e48871378
Yang Li <odielee@hotmail.com>
2022-05-20 14:04:52 -0400

    Form structure

--- frontend/src/pages/createOrder/createOrder.jsx
+++ frontend/src/pages/createOrder/createOrder.jsx
@@ -1,6 +1,38 @@
 const createOrder = () => {
   return (
-    <div>createOrder</div>
+    <div>
+      <form className="Form">
+        <div className="Form-Name">
+          <label className="Label-Name">Name</label>
+          <input type="text" className="Input-Name" />
+        </div>
+
+        <div className="Form-Weight">
+          <label className="Label-Weight">Weight</label>
+          <input type="text" className="Input-Weight" />
+        </div>
+
+        <div className="Form-Colour">
+          <label className="Label-Colour">Colour</label>
+          <input type="color" className="Input-Colour" />
+        </div>
+
+        <div className="Form-Country">
+          <label className="Form-Country">Country</label>
+          <select className="Select-Country" defaultValue='default'>
+            <option value="default" disabled>Select Country</option>
+            <option value="sweden">Sweden</option>
+            <option value="china">China</option>
+            <option value="brazil">Brazil</option>
+            <option value="australia">Australia</option>
+          </select>
+        </div>
+
+        <div className="Form-SaveBtn">
+          <button>Save</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
