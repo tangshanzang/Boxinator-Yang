fdce74f054f3ab45947d5b28cdf047d893003151
Yang Li <odielee@hotmail.com>
2022-05-22 23:30:27 -0400

    CSS

--- frontend/src/component/orderDetails/orderDetails.jsx
+++ frontend/src/component/orderDetails/orderDetails.jsx
@@ -1,23 +1,40 @@
 import React from 'react'
+import './orderDetails.css'
 
 const orderDetails = (props) => {
   return (
-    <div>
+    <div className='detailContainer'>
       <div className="receiver">
-        <p>Receiver</p>
-        <p>{props.order.name}</p>
+        <div className="title">
+          <p>Receiver</p>
+        </div>
+        <div className="receiver-Value">
+          <p>{props.order.name}</p>
+        </div>
       </div>
       <div className="weight">
-        <p>Weight</p>
-        <p>{props.order.weight + ' kilograms'}</p>
+        <div className="title">
+          <p>Weight</p>
+        </div>
+        <div className="weight-Value">
+          <p>{props.order.weight + ' kilograms'}</p>
+        </div>
       </div>
       <div className="boxColour">
-        <p>Box colour</p>
-        <p>{props.order.colour}</p>
+        <div className="title">
+          <p>Box colour</p>
+        </div>
+        <div className="boxColour-Value">
+          <p>{props.order.colour}</p>
+        </div>
       </div>
       <div className="shippingCost">
-        <p>Shipping cost</p>
-        <p>{props.order.cost + ' SEK'}</p>
+        <div className="title">
+          <p>Shipping cost</p>
+        </div>
+        <div className="shippingCost-Value">
+          <p>{props.order.cost + ' SEK'}</p>
+        </div>
       </div>
     </div>
   )
